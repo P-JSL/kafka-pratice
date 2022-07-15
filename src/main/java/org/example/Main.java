a769cd387c9e6387f3d994b1769ec0aff5f592bf
P-JSL <althsuwpfl@gmail.com>
2022-07-15 16:08:57 +0900

    Changes

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092"); // kafka server host 및 port
+        props.put("session.timeout.ms", "10000"); // session 설정
+        props.put("group.id", "quickstart-events");   // topic 설정
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");    // key deserializer
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");  // value deserializer
+
+        Producer<String,String> producer = new KafkaProducer<>(props);
+        producer.send(new ProducerRecord<>("peter-topic","Apache Kafka"));
+        producer.close();
+    }
+}
